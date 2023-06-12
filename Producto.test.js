46e694bf8cec7681d22d3a2418b8b7bbc7352511
Martin Di Cesare <94543558+MartinDiCesare@users.noreply.github.com>
2023-06-12 11:44:48 -0300

    Update Producto.test.js

--- Producto.test.js
+++ Producto.test.js
@@ -2,10 +2,10 @@ const Producto = require('./Producto');
 
 test('El precio del producto no debe ser menor a 0', () => {
   const producto1 = new Producto('Camiseta', 10);
-  expect(producto1.precio).toBe(10);
+  expect(producto1.getPrecio).toBe(10);
 
   const producto2 = new Producto('Zapatos', 0);
-  expect(producto2.precio).toBe(0);
+  expect(producto2.getPrecio).toBe(0);
 
   expect(() => {
     new Producto('Pantalones', -5);
