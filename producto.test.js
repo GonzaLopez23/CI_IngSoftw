dd0949606c95ec215aeebf6496a289bee396b021
GonzaLopez23 <72515679+GonzaLopez23@users.noreply.github.com>
2023-06-11 22:00:00 -0300

    Update producto.test.js

--- producto.test.js
+++ producto.test.js
@@ -3,3 +3,10 @@ test('testGetNombre', () => {
   // Verifica que el método getNombre devuelve 'Remera'
   expect(producto1.getNombre()).toBe('Remera');
 });
+
+test('testSetNombre', () => {
+  const producto1 = new Producto();
+  producto1.setNombre('Zapatilla');
+  // Verifica que el método setNombre actualice correctamente el nombre
+  expect(producto1.getNombre()).toBe('Zapatilla');
+});
