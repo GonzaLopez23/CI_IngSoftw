a984afd4db8c9436ae4e4a7b51e98939a9eece72
Martin Di Cesare <94543558+MartinDiCesare@users.noreply.github.com>
2023-06-11 21:54:08 -0300

    Update producto.test.js

--- producto.test.js
+++ producto.test.js
@@ -1,6 +1,6 @@
-require('./producto.js')
+
 test('Validar precio de producto positivo', () => {
-const productoCreado = new Producto();
+const productoCreado = require('./producto.js');
 expect(() => {
   productoCreado.setPrecio(-100.0); // asignamos un precio negativo
  }).toThrow('El precio del producto no debe ser menor a 0');
