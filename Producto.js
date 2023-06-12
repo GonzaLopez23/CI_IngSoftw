f7ff4884569852531f1fe5fbc00e0da2265d11e5
Martin Di Cesare <94543558+MartinDiCesare@users.noreply.github.com>
2023-06-12 12:00:42 -0300

    Update Producto.js

--- Producto.js
+++ Producto.js
@@ -1,17 +1,25 @@
 class Producto {
   constructor(nombre, precio) {
     this.nombre = nombre;
-    this.precio = this.setPrecio(precio);
+    setPrecio(precio);
   }
-
+  constructor () {
+  }
   setPrecio(precio) {
     if (precio < 0) {
       throw new Error('El precio no debe ser menor a 0.');
     }
-    return precio;
+    this.precio = precio;
   }
   getPrecio () {
     return this.precio;
+  }
+  setDescripcion(descripcion){
+    if (descripcion.length < 10 || descripcion.length > 256) {
+      throw new Error('La descripcion del producto no debe tener más de 256 caracteres ni menos de 10 caracteres.');
+    } else {
+      this.descripcion = descripcion; 
+    }
   }
 }
 
