343d5f1e1bde19c2d5d514a4864b43c030d5420c
Martin Di Cesare <94543558+MartinDiCesare@users.noreply.github.com>
2023-06-11 22:22:55 -0300

    Update and rename producto.test.js to Producto.test.js

---
+++ Producto.test.js
@@ -0,0 +1,13 @@
+const Producto = require('./Producto');
+
+test('El precio del producto no debe ser menor a 0', () => {
+  const producto1 = new Producto('Camiseta', 10);
+  expect(producto1.precio).toBe(10);
+
+  const producto2 = new Producto('Zapatos', 0);
+  expect(producto2.precio).toBe(0);
+
+  expect(() => {
+    new Producto('Pantalones', -5);
+  }).toThrowError('El precio no puede ser menor a 0.');
+});
