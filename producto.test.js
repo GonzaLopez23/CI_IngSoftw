11658853742b30f1a76a1f6bd8b2eb03cc6b371a
matias9718 <136196493+matias9718@users.noreply.github.com>
2023-06-12 09:40:43 -0300

    Update producto.test.js

--- producto.test.js
+++ producto.test.js
@@ -1,13 +1,13 @@
 const Producto = require('./producto');
 
 test('testGetNombre', () => {
-  const producto1 = new Producto();
+  const producto1 = new Producto("Remera");
   // Verifica que el método getNombre devuelve 'Remera'
   expect(producto1.getNombre()).toBe('Remera');
 });
 
 test('testSetNombre', () => {
-  const producto1 = new Producto();
+  const producto1 = new Producto('Zapatilla');
   producto1.setNombre();
   // Verifica que el método setNombre actualice correctamente el nombre
   expect(producto1.getNombre()).toBe('Zapatilla');
