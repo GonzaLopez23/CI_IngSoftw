b4197a328e3b80c214282eaa1f25ccea9631043a
Martin Di Cesare <94543558+MartinDiCesare@users.noreply.github.com>
2023-06-12 12:43:36 -0300

    Update Producto.test.js

--- Producto.test.js
+++ Producto.test.js
@@ -3,10 +3,17 @@ const Producto = require('./Producto');
 test('El precio del producto no debe ser menor a 0', () => {
   const producto1 = new Producto();
   producto1.setPrecio(10);
-  expect(producto1.precio).toBe(10);
+  expect(producto1.getPrecio()).toBe(10);
 
   expect(() => {
-    const producto2 new Producto();
+    const producto2 = new Producto();
     producto2.setPrecio(-5);
   }).toThrowError('El precio no debe ser menor a 0.');
+});
+
+test('Validar longitud de la descripcion del producto', () => {
+  expect(() => {
+    const producto1 = new Producto();
+    producto1.setDescripcion("");
+  }).toThrowError('La descripcion del producto no debe tener más de 256 caracteres ni menos de 10 caracteres.');
 });
