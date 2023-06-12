64b283e6dec05b7f25ada5c789d6f077960f3fe6
Martin Di Cesare <94543558+MartinDiCesare@users.noreply.github.com>
2023-06-12 13:18:20 -0300

    Update Producto.test.js

--- Producto.test.js
+++ Producto.test.js
@@ -13,3 +13,11 @@ test('Descripcion del producto menor a 10 caracteres devuelve error', () => {
     producto1.setDescripcion("");
   }).toThrowError('La descripcion del producto no debe tener más de 256 caracteres ni menos de 10 caracteres.');
 });
+
+test('Descripcion del producto menor a 10 caracteres devuelve error', () => {
+  const producto1 = new Producto();
+  producto1.setDescripcion("Zapatillas");
+  const description = producto1.getDescripcion();
+  expect(typeof description).toBeInstanceOf('string'
+  
+});
