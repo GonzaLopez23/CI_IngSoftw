cbe880dcf8f4316aea11ea51f455540bd4ae6d37
Martin Di Cesare <94543558+MartinDiCesare@users.noreply.github.com>
2023-09-30 20:29:34 -0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,6 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   plugins: ['prettier'],
   rules: {
-    semi: ['error', 'always'],
-    quotes: ['error', 'single'],
-    indent: ['error', 2],
-    'operator-linebreak': ['error', 'before'],
     'max-len': ['error', { 'code': 120 }],
-    'no-trailing-spaces': 'error',
-    'no-multi-spaces': 'error',
-    'no-unused-vars': 'error',
-    'no-console': 'error',
   },
 };
