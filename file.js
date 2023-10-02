4d6d37a5f439867c02b5eb2169ded4d4616d99b9
GonzaLopez23 <72515679+GonzaLopez23@users.noreply.github.com>
2023-10-02 12:46:21 -0300

    Update file.js

--- file.js
+++ file.js
@@ -1,10 +1,8 @@
-const longString =
-  'Esta es una cadena de texto muy larga que debería ser formateada de acuerdo a las reglas que estableciste en el archivo .prettierrc.json';
+const longString ='Esta es una cadena de texto muy larga que debería ser formateada de acuerdo a las reglas que estableciste en el archivo .prettierrc.json';
 
 function ejemploFuncion() {
   if (longString.length > 40) {
-    console.log(
-      'Esta función tiene una línea que excede los 40 caracteres permitidos.',
+    console.log('Esta función tiene una línea que excede los 40 caracteres permitidos.',
     );
   } else {
     console.log('Todo está bien.');
