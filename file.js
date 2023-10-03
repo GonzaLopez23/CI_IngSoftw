b39cfcdc89e13867d2b9db7bd1fe454832c51623
DiegoBustos16 <115572743+DiegoBustos16@users.noreply.github.com>
2023-10-03 11:33:49 -0300

    Update file.js

--- file.js
+++ file.js
@@ -1,10 +1,1 @@
-function ejemploFuncion() {
-  if (longString.length > 40) {
-    console.log(
-      'Esta función tiene una línea',
-    );
-  } else {
-    console.log('Todo está bien.');
-  }
-}
-ejemploFuncion();
+
