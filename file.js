0321cf2b0e8df0d46f005a03ff019f536060c308
GonzaLopez23 <72515679+GonzaLopez23@users.noreply.github.com>
2023-10-04 12:05:27 -0300

    Update file.js

--- file.js
+++ file.js
@@ -2,6 +2,7 @@ const longString =
   'Esta es una cadena de texto muy larga que debería ser formateada de acuerdo a las reglas que estableciste en el archivo .prettierrc.json';
 let x = 10;
 
+// prettier-ignore
 if (x = 20) {
   console.log('x es igual a 20');
 }
