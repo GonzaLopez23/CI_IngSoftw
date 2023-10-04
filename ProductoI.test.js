7250401b255733b2cf66e919309c6405bd5c9987
DiegoBustos16 <115572743+DiegoBustos16@users.noreply.github.com>
2023-10-04 18:33:19 -0300

    Update ProductoI.test.js

--- ProductoI.test.js
+++ ProductoI.test.js
@@ -1,40 +1,40 @@
 const Producto = require('./Producto'); // Asegúrate de que la ruta sea correcta
 
-describe('Producto Integration Test', () => {
+describe('Prueba de Integración de Producto', () => {
   let producto;
 
   beforeEach(() => {
     producto = new Producto();
   });
 
-  it('should set and get the nombre correctly', () => {
+  it('debería establecer y obtener el nombre correctamente', () => {
     producto.setNombre('Producto de Prueba');
     expect(producto.getNombre()).toBe('Producto de Prueba');
   });
 
-  it('should set and get the precio correctly', () => {
+  it('debería establecer y obtener el precio correctamente', () => {
     producto.setPrecio(25.99);
     expect(producto.getPrecio()).toBe(25.99);
   });
 
-  it('should throw an error when precio is less than 0', () => {
+  it('debería lanzar un error cuando el precio es menor que 0', () => {
     expect(() => producto.setPrecio(-10)).toThrow(
       'El precio no debe ser menor a 0.'
     );
   });
 
-  it('should set and get the descripcion correctly', () => {
+  it('debería establecer y obtener la descripción correctamente', () => {
     producto.setDescripcion('Descripción de prueba');
     expect(producto.getDescripcion()).toBe('Descripción de prueba');
   });
 
-  it('should set and get the marca correctly', () => {
+  it('debería establecer y obtener la marca correctamente', () => {
     producto.setMarca('Marca de Prueba');
     expect(producto.getMarca()).toBe('Marca de Prueba');
   });
 
-  it('should throw an error when marca length is greater than 15', () => {
-    expect(() => producto.setMarca('A Very Long Brand Name')).toThrow(
+  it('debería lanzar un error cuando la longitud de la marca es mayor que 15', () => {
+    expect(() => producto.setMarca('Un Nombre de Marca Muy Largo')).toThrow(
       'La marca del producto no debe tener más de 15 caracteres.'
     );
   });
