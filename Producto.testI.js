36695d2d9ac022a9eca8b8672e8cd03be8688a36
DiegoBustos16 <115572743+DiegoBustos16@users.noreply.github.com>
2023-10-04 18:02:14 -0300

    Create Producto.testI.js

---
+++ Producto.testI.js
@@ -0,0 +1,51 @@
+const Producto = require('./Producto'); // Asegúrate de que la ruta sea correcta
+
+describe('Producto Integration Test', () => {
+  let producto;
+
+  beforeEach(() => {
+    producto = new Producto();
+  });
+
+  it('should set and get the nombre correctly', () => {
+    producto.setNombre('Producto de Prueba');
+    expect(producto.getNombre()).toBe('Producto de Prueba');
+  });
+
+  it('should set and get the precio correctly', () => {
+    producto.setPrecio(25.99);
+    expect(producto.getPrecio()).toBe(25.99);
+  });
+
+  it('should throw an error when precio is less than 0', () => {
+    expect(() => producto.setPrecio(-10)).toThrow(
+      'El precio no debe ser menor a 0.'
+    );
+  });
+
+  it('should set and get the descripcion correctly', () => {
+    producto.setDescripcion('Descripción de prueba');
+    expect(producto.getDescripcion()).toBe('Descripción de prueba');
+  });
+
+  it('should throw an error when descripcion length is out of bounds', () => {
+    expect(() => producto.setDescripcion('Short')).toThrow(
+      'La descripcion del producto no debe tener más de 256 caracteres ni menos de 10 caracteres.'
+    );
+
+    expect(() => producto.setDescripcion('This is a very long description that exceeds the 256 character limit.')).toThrow(
+      'La descripcion del producto no debe tener más de 256 caracteres ni menos de 10 caracteres.'
+    );
+  });
+
+  it('should set and get the marca correctly', () => {
+    producto.setMarca('Marca de Prueba');
+    expect(producto.getMarca()).toBe('Marca de Prueba');
+  });
+
+  it('should throw an error when marca length is greater than 15', () => {
+    expect(() => producto.setMarca('A Very Long Brand Name')).toThrow(
+      'La marca del producto no debe tener más de 15 caracteres.'
+    );
+  });
+});
